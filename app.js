fb06aaf3c06aa5856e77c0c80fd1b4d290b906b2
Faraz <farazatgithub@gmail.com>
2023-04-05 14:23:44 +0500

    mongoose and mongodb

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const express = require("express")
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const tasksRouter = require("./routes/tasks.routes")
+const tasksRouter = require("./routes/tasks.routes");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const MONGO_URL=process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
 
-app.use('/api/v1/tasks', tasksRouter)
+app.use("/api/v1/tasks", tasksRouter);
 
-app.listen(PORT,() => {
-    console.log(`listening to server at port ${PORT}`)
-})
+mongoose.connection.on("open", () => {
+  console.log("mongoose conncted successfully");
+});
 
+// mongoose.connection.on("error", () => {
+//     console.log('could not connect to mongo database')
+// })
 
+async function startServer() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`listening to server at port ${PORT}`);
+    });
+  } catch (err) {
+    console.log('could not connect to database');
+  }
+}
 
-
+startServer();
