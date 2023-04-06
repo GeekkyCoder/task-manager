b29c96ffaed187bbf870d4a1458d3afc04e2450c
Faraz <farazatgithub@gmail.com>
2023-04-06 20:08:19 +0500

    async wrapper for controllers

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,57 +1,38 @@
 const Task = require("../modal/tasks.modal");
+const asyncWrapper = require("../middlewares/asyn-wrapper")
 
-async function getAllTasks(req, res) {
-  try {
-    const tasks = await Task.find({});
-    return res.status(200).json({ tasks });
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  return res.status(200).json({ tasks });
+});
 
-async function createTask(req, res) {
-  try {
-    const task = await Task.create(req.body);
-    return res.status(201).json(task);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  return res.status(201).json(task);
+});
 
-async function getATask(req, res) {
+const getATask = asyncWrapper(async (req, res) => {
   const taskId = req.params.id;
-  try {
-    const task = await Task.findOne({ _id: taskId });
-    if (!task) return res.status(404).json({ msg: "task does not exist" });
-    return res.status(200).json({ task });
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
-}
+  const task = await Task.findOne({ _id: taskId });
+  if (!task) return res.status(404).json({ msg: "task does not exist" });
+  return res.status(200).json({ task });
+});
 
-async function updateATask(req, res) {
+const updateATask = asyncWrapper(async (req, res) => {
   const taskId = req.params.id;
   const payload = req.body;
-  try {
-    await Task.findOneAndUpdate({ _id: taskId }, payload, {
-      runValidators: true,
-    });
-    const task = await Task.findOne({ _id: taskId });
-    return res.status(201).json({ task });
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
-}
+  await Task.findOneAndUpdate({ _id: taskId }, payload, {
+    runValidators: true,
+  });
+  const task = await Task.findOne({ _id: taskId });
+  return res.status(201).json({ task });
+});
 
-async function deleteATask(req, res) {
+const deleteATask = asyncWrapper(async (req, res) => {
   const taskId = req.params.id;
-  try {
-    const deletedTask = await Task.findOneAndDelete({ _id: taskId });
-    return res.status(200).json(deletedTask);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
-}
+  const deletedTask = await Task.findOneAndDelete({ _id: taskId });
+  return res.status(200).json(deletedTask);
+});
 
 module.exports = {
   getAllTasks,
